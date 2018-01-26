2b286a3fdf4cd90a4e257957cf5f9286970d46a7
CSharpFri <CSharpFri@CLASS-2-11>
2018-01-26 17:34:13 +0100

    k

---
+++ delegates/item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegates
+{
+    class item
+    {
+        string meno;
+        int pocetkusov;
+        double cenazakus;
+
+    }
+}
